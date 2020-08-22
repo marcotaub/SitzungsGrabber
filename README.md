2c9cee4532faa7106e8bdec7a5d1ae4cf81f97af
marcotaub <marcotaub@gmail.com>
2020-08-22 16:08:15 +0200

    ➕ added Rat & Member Fetch

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-
+## SitzungsGrabber
+###### Ein Tool um die öffentlich zugänglichen Daten der Gemeinderatsseite in NODE JS abfragen zu können
+**Dieses Projekt ist nur ein Sandbox Projekt**
--- server/src/scrape.js
+++ server/src/scrape.js
@@ -1,36 +1,36 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
-const base_url = "http://buergerinfo.bernhardswald.de/";
-const rat_mitglieder_url = "http://buergerinfo.bernhardswald.de/kp0040.asp?__kgrnr=1";
-const rat_sitzungen_url = "http://buergerinfo.bernhardswald.de/si0041.asp?__ctopic=gr&__kgrnr=1";
+const baseUrl = 'http://buergerinfo.bernhardswald.de/';
+const ratMitgliederUrl = 'http://buergerinfo.bernhardswald.de/kp0040.asp?__kgrnr=1';
+const ratSitzungenUrl = 'http://buergerinfo.bernhardswald.de/si0041.asp?__ctopic=gr&__kgrnr=1';
 
-async function fetchRatMembers(){
-  const response = await fetch(rat_mitglieder_url);
+async function fetchRatMembers() {
+  const response = await fetch(ratMitgliederUrl);
   const html = await response.text();
   const $ = cheerio.load(html);
 
-  const members_container = $('tbody > tr');
-  let members = [];
-  for(let i = 0; i< members_container.length; i++){
-    const member_container = $(members_container[i]);
-    const member_name = $(member_container).find('a').text();
-    const member_party = $(member_container).find('td.smc_td.smc_field_pepartei').text();
-    const member_funktion = $(member_container).find('td.smc_td.smc_field_mgfunk').text().trim();
+  const membersContainer = $('tbody > tr');
+  const members = [];
+  for (let i = 0; i < membersContainer.length; i += 1) {
+    const memberContainer = $(membersContainer[i]);
+    const memberName = $(memberContainer).find('a').text();
+    const memberParty = $(memberContainer).find('td.smc_td.smc_field_pepartei').text();
+    const memberFunktion = $(memberContainer).find('td.smc_td.smc_field_mgfunk').text().trim();
 
-    const member_profile_url = $(member_container).find('a').attr('href');
-    const response_member_profile = await fetch(base_url+member_profile_url);
-    const html_member_profile = await response_member_profile.text();
-    const b = cheerio.load(html_member_profile);
-    const member_profile_pic_url = b('#smccontent').find('img.smcimgperson').attr('src');
+    const memberProfileUrl = $(memberContainer).find('a').attr('href');
+    const responseMemberProfile = await fetch(baseUrl + memberProfileUrl);
+    const htmlMemberProfile = await responseMemberProfile.text();
+    const b = cheerio.load(htmlMemberProfile);
+    const memberProfilePicUrl = b('#smccontent').find('img.smcimgperson').attr('src');
 
-    if(member_name != ''){
+    if (memberName !== '') {
       members.push({
-        member_name,
-        member_party,
-        member_funktion,
-        member_profile_url,
-        member_profile_pic_url,
+        memberName,
+        memberParty,
+        memberFunktion,
+        memberProfileUrl,
+        memberProfilePicUrl,
       });
     }
   }
@@ -38,57 +38,54 @@ async function fetchRatMembers(){
   console.log(members);
 }
 
-async function fetchRatSitzungen(){
-  const response = await fetch(rat_sitzungen_url);
+async function fetchRatSitzungen() {
+  const response = await fetch(ratSitzungenUrl);
   const html = await response.text();
   const $ = cheerio.load(html);
 
   const sitzungenContainer = $('tbody > tr');
-  let sitzungen = [];
-  for (let i = 0; i < sitzungenContainer.length; i++) {
-    const sitzungContainer = $(sitzungenContainer[i]); 
-    
-    //ORT, Zeit
+  const sitzungen = [];
+  for (let i = 0; i < sitzungenContainer.length; i += 1) {
+    const sitzungContainer = $(sitzungenContainer[i]);
+
+    // ORT, Zeit
     const sitzungOrt = sitzungContainer.find('.smc_field_siort').text();
     const sitzungZeitpunkt = sitzungContainer.find('.smc_field_silink').text();
     const sitzungZeit = sitzungZeitpunkt.slice(10).trim();
-    
-    //Datum, isDone
-    const sitzungDateArray = sitzungZeitpunkt.slice(0,10).trim().split('.');
-    let sitzungDatum = sitzungZeitpunkt.slice(0,10).trim();
+
+    // Datum, isDone
+    const sitzungDateArray = sitzungZeitpunkt.slice(0, 10).trim().split('.');
+    let sitzungDatum = sitzungZeitpunkt.slice(0, 10).trim();
     let sitzungDone = false;
-    if(sitzungDateArray != ''){
+    if (sitzungDatum !== '') {
       const now = new Date();
-      console.log(sitzungDateArray);
-      sitzungDatum = new Date(sitzungDateArray[2]+"-"+sitzungDateArray[1]+"-"+sitzungDateArray[0]+" 21:30");
-      
-      if(sitzungDatum < now){
+      sitzungDatum = new Date(`${sitzungDateArray[2]}-${sitzungDateArray[1]}-${sitzungDateArray[0]} 21:30`);
+
+      if (sitzungDatum < now) {
         sitzungDone = true;
       }
     }
-    
-    //Link
+
+    // Link
     const sitzungLink = sitzungContainer.find('td.smc_td.smc_field_silink > a').attr('href');
 
-    //TOPS
-    let tops = [];
-    if(sitzungLink != ""){
-      const response_top = await fetch(base_url+sitzungLink);
-      const html_top = await response_top.text();
-      const t = cheerio.load(html_top);
-      const tops_container = $('tbody > tr');
+    // TOPS
+    const tops = [];
+    if (sitzungLink !== '') {
+      const responseTop = await fetch(baseUrl + sitzungLink);
+      const htmlTop = await responseTop.text();
+      const t = cheerio.load(htmlTop);
+      const topsContainer = t('#smc_page_to0040_contenttable1 > tbody > tr');
 
-      for (let j = 0; j < tops_container.length; j++) {
-        const onetop = tops_container.find('td.smcrow1.smc_topht.smc_topht1').text();
-
-        tops.push({
-          onetop,
-        })
+      for (let j = 0; j < topsContainer.length; j += 1) {
+        const topsTrs = t(topsContainer[j]).find('td.smc_topht').text();
+        if (topsTrs !== '') {
+          tops.push(topsTrs);
+        }
       }
-    }  
+    }
 
-
-    if(sitzungZeitpunkt != ''){
+    if (sitzungZeitpunkt !== '') {
       sitzungen.push({
         sitzungOrt,
         sitzungZeit,
@@ -102,5 +99,6 @@ async function fetchRatSitzungen(){
 
   console.log(sitzungen);
 }
-//fetchRatMembers();
+
+fetchRatMembers();
 fetchRatSitzungen();
